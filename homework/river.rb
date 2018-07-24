9b4fb9a4e45e80328775b2dd7d071314455b5ff7
Fiona Wilson <fionaw237@gmail.com>
2018-07-24 21:42:09 +0100

    minor changes

--- homework/river.rb
+++ homework/river.rb
@@ -7,6 +7,10 @@ class River
     @fish = []
   end
 
+  def fish_count()
+    return @fish.count()
+  end
+
   def add_fish_to_river(fish)
     @fish.concat(fish)
   end
@@ -14,15 +18,5 @@ class River
   def remove_a_fish()
     @fish.pop()
   end
-
-  def fish_count()
-    return @fish.count()
-  end
-
-  def fish()
-    copy = []
-    copy.replace(@fish)
-  end
-
 
 end
