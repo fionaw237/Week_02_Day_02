a6e92a4c7be12447e49416656aafae582e8b1da3
Fiona Wilson <fionaw237@gmail.com>
2018-07-24 18:19:40 +0100

    adds test for name

--- homework/specs/fish_spec.rb
+++ homework/specs/fish_spec.rb
@@ -5,7 +5,11 @@ require_relative("../fish")
 class FishTest < MiniTest::Test
 
   def setup()
+    @fish = Fish.new("Freddie")
+  end
 
+  def test_has_name()
+    assert_equal("Freddie", @fish.name())
   end
 
 end
