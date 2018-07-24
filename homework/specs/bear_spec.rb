b5c99da82cfb6f42cdaf2ae8a5a1f10863f65709
Fiona Wilson <fionaw237@gmail.com>
2018-07-24 18:16:14 +0100

    Adds tests for name and type

--- homework/specs/bear_spec.rb
+++ homework/specs/bear_spec.rb
@@ -6,6 +6,16 @@ class BearTest < MiniTest::Test
 
   def setup()
 
+    @bear = Bear.new("Sparky", "brown bear")
+
+  end
+
+  def test_has_name()
+    assert_equal("Sparky", @bear.name)
+  end
+
+  def test_has_type()
+    assert_equal("brown bear", @bear.type)
   end
 
 end
