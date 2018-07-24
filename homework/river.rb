0d489a2a24ef40c78e89cc8e4b9cb51d3220cbfe
Fiona Wilson <fionaw237@gmail.com>
2018-07-24 18:34:43 +0100

    Adds function for adding fish to river, and appropriate test

--- homework/river.rb
+++ homework/river.rb
@@ -1,9 +1,15 @@
 class River
 
-  attr_reader :name
+  attr_reader :name, :fish
 
   def initialize(name)
     @name = name
+    @fish = []
   end
+
+  def add_fish_to_river(fish)
+    @fish.concat(fish)
+  end
+
 
 end
--- homework/specs/river_spec.rb
+++ homework/specs/river_spec.rb
@@ -1,6 +1,7 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../river")
+require_relative("../fish")
 
 class RiverTest < MiniTest::Test
 
@@ -8,10 +9,22 @@ class RiverTest < MiniTest::Test
 
     @river = River.new("Tay")
 
+    @fish1 = Fish.new("fish1")
+    @fish2 = Fish.new("fish2")
+    @fish3 = Fish.new("fish3")
+    @fish4 = Fish.new("fish4")
+
+    @fish = [@fish1, @fish2, @fish3, @fish4]
+
   end
 
   def test_has_name()
     assert_equal("Tay", @river.name())
+  end
+
+  def test_add_fish_to_river()
+    @river.add_fish_to_river(@fish)
+    assert_equal(4, @river.fish.length())
   end
 
 end
